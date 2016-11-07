14066cf6d641ebffd48f43a3e316e3a1343c49c2
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-11-07 15:38:51 +0100

    driver lovi passgen

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,5 +1,21 @@
+import string
+import random
+
+
 def passwordgen():
-    return
+    lowerCaseChar = string.ascii_lowercase[:26]
+    upperCaseChar = string.ascii_uppercase[:26]
+    num = string.digits[:10]
+    specChar = "!@#$%^&*()?"
+    theList = [lowerCaseChar, upperCaseChar, num, specChar]
+    pw = ""
+    for i in range(0, 3):
+        for b in theList:
+            randChar = random.randint(0, len(b) - 1)
+            randPw = random.randint(0, len(pw))
+            pw += b[randChar]
+    print(pw)
+    return pw
 
 
 def main():
